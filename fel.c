0c67e1ec16a4efacb6a2c829094046f9b5b8f062
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2015-12-17 12:44:54 +0200

    fel: Rename the backup buffers which use AR100 (OpenRISC) SRAM
    That would be a more appropriate name. And A31 is going to
    implement this in a different way and give the SRAM back to
    OpenRISC.
    
    Signed-off-by: Siarhei Siamashka <siarhei.siamashka@gmail.com>

--- fel.c
+++ fel.c
@@ -479,12 +479,11 @@ sram_swap_buffers a10_a13_a20_sram_swap_buffers[] = {
 };
 
 /*
- * A31 is very similar to A10/A13/A20, except that it has no SRAM at 0x8000.
- * So we use the SRAM section at 0x44000 instead. This is the memory, which
- * is normally shared with the OpenRISC core (should we do an extra check to
- * ensure that this core is powered off and can't interfere?).
+ * Use the SRAM section at 0x44000 as the backup storage. This is the memory,
+ * which is normally shared with the OpenRISC core (should we do an extra check
+ * to ensure that this core is powered off and can't interfere?).
  */
-sram_swap_buffers a31_sram_swap_buffers[] = {
+sram_swap_buffers ar100_abusing_sram_swap_buffers[] = {
 	{ .buf1 = 0x01800, .buf2 = 0x44000, .size = 0x800 },
 	{ .buf1 = 0x05C00, .buf2 = 0x44800, .size = 0x8000 - 0x5C00 },
 	{ 0 }  /* End of the table */
@@ -515,32 +514,32 @@ soc_sram_info soc_sram_info_table[] = {
 		.soc_id       = 0x1650, /* Allwinner A23 */
 		.scratch_addr = 0x2000,
 		.thunk_addr   = 0x46E00, .thunk_size = 0x200,
-		.swap_buffers = a31_sram_swap_buffers,
+		.swap_buffers = ar100_abusing_sram_swap_buffers,
 	},
 	{
 		.soc_id       = 0x1633, /* Allwinner A31 */
 		.scratch_addr = 0x2000,
 		.thunk_addr   = 0x46E00, .thunk_size = 0x200,
-		.swap_buffers = a31_sram_swap_buffers,
+		.swap_buffers = ar100_abusing_sram_swap_buffers,
 	},
 	{
 		.soc_id       = 0x1667, /* Allwinner A33 */
 		.scratch_addr = 0x2000,
 		.thunk_addr   = 0x46E00, .thunk_size = 0x200,
-		.swap_buffers = a31_sram_swap_buffers,
+		.swap_buffers = ar100_abusing_sram_swap_buffers,
 	},
 	{
 		.soc_id       = 0x1673, /* Allwinner A83T */
 		.scratch_addr = 0x2000,
 		.thunk_addr   = 0x46E00, .thunk_size = 0x200,
-		.swap_buffers = a31_sram_swap_buffers,
+		.swap_buffers = ar100_abusing_sram_swap_buffers,
 	},
 	{
 		.soc_id       = 0x1680, /* Allwinner H3 */
 		.scratch_addr = 0x2000,
 		.mmu_tt_addr  = 0x44000,
 		.thunk_addr   = 0x46E00, .thunk_size = 0x200,
-		.swap_buffers = a31_sram_swap_buffers,
+		.swap_buffers = ar100_abusing_sram_swap_buffers,
 	},
 	{ 0 } /* End of the table */
 };
